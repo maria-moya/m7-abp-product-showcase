9d41693b7e9266addf25a8579b1527e9c0503657
Maria Moya <mariamoya9425@gmail.com>
2026-03-24 19:01:06 -0300

    Se modifica el componente NavBar.vue para mejor estetica

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container">
         <a class="navbar-brand" href="#">Product ShowCase</a>
         <button
@@ -49,7 +49,51 @@
           </div>
         </div>
       </div>
+    </nav> -->
+  
+    <nav class="navbar navbar-expand-lg navbar-light bg-withe sticky-top py-3">
+      <div class="container position-relative justify-content-center">
+        
+        <div class="position-absolute start-0 d-none d-md-block">
+          <v-btn icon="mdi-magnify" variant="text" density="confortable" color="grey-darken-1"></v-btn>
+        </div>
+
+        <div class="d-flex gap-4 align-center">
+          <RouterLink to="/" class="nav-link-custom">Inicio</RouterLink>
+          <RouterLink to="/about" class="nav-link-custom">Nosotros</RouterLink>
+          <RouterLink v-if="isAdmin" :to="{ name: 'crud-products' }" class="nav-link-custom">Crud productos</RouterLink>
+          <RouterLink v-if="isAuth" to="/favorites" class="nav-link-custom ">Favoritos</RouterLink>
+        </div>
+
+        <div class="position-absolute end-0 d-flex align-center gap-2">
+          <template v-if="!isAuth">
+            <RouterLink to="/login" class="user-link">
+              <v-icon size="small" class="me-1">mdi-account-outline</v-icon>
+              <span>Inicia sesión</span>
+            </RouterLink>
+          </template>
+
+          <template v-else>
+            <span class="text-caption me-2 d-none d-sm-inline">Hola, {{ displayName }}</span>
+            <v-btn icon="mdi-logout-variant" variant="text" size="small" @click="onLogout"></v-btn>
+          </template>
+
+          <v-btn icon variant="text" density="comfortable">
+            <v-badge content="0" color="black" offset-x="3" offset-y="3">
+              <v-icon size="small">mdi-cart-outline</v-icon>
+            </v-badge>
+          </v-btn>
+
+          <v-btn icon variant="text" density="comfortable" @click="themeStore.toggleTheme()">
+            <v-icon size="small">
+              {{ themeStore.isDarkMode ? 'mid-moon-waning-crescent' : 'mid-white-balance-sunny' }}
+            </v-icon>
+          </v-btn>
+        </div>
+
+      </div>
     </nav>
+  
   </div>
 </template>
 
@@ -85,4 +129,30 @@ async function onLogout() {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="css">
+.nav-link-custom{
+  text-decoration: none;
+  color: #555;
+  font-size: 1rem;
+  letter-spacing: 1px;
+  text-transform: capitalize;
+  transition: color 0.3s;
+}
+
+.nav-lik-custom:hover{
+  color: #000;
+}
+
+.user-link{
+  text-decoration: none;
+  color: #666;
+  font-size: 0.75rem;
+  display: flex;
+  align-items: center;
+}
+
+.router-link-active{
+  font-weight: 500;
+  color: #000 !important;
+}
+</style>
