60755a9a825e749f205d63ccfd0ac72b61e29961
Maria Moya <mariamoya9425@gmail.com>
2026-03-28 19:04:58 -0300

    Se modifica los archivos para hacer deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import { useUserStore } from '@/stores/user.store'
 import { auth } from '@/firebaseConfig'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
