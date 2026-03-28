076c9076e709cd350fc82a3f60a24d7e8ff3139c
Maria Moya <mariamoya9425@gmail.com>
2026-03-28 18:49:44 -0300

    Mejoras en el proyecto

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/m7-abp-product-showcase/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
